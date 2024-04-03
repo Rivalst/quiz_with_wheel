57eb440b2de836b56bc31d491a2efff28a9f9727
bohdan54844460 <bohdan@horda.dev>
2024-04-03 19:32:02 +0300

    Refactor

--- lib/src/features/fortune_wheel/widgets/helpers/result.dart
+++ lib/src/features/fortune_wheel/widgets/helpers/result.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quiz_with_wheel/src/features/fortune_wheel/bloc/bloc/fortune_bloc.dart';
 import 'package:quiz_with_wheel/src/features/fortune_wheel/data/model/fortune_item_model.dart';
+import 'package:quiz_with_wheel/src/features/quiz/bloc/quiz_score_bloc.dart';
 
 class ResultWidget extends StatefulWidget {
   final List<FortuneItem> items;
@@ -27,17 +28,29 @@ class _ResultWidgetState extends State<ResultWidget> {
   @override
   Widget build(BuildContext context) {
     final index = _calIndex(widget.value * widget.angle + widget.current);
-    String asset = widget.items[index].name;
+    final item = widget.items[index].name;
+    final score = context.read<QuizScoreBloc>().state.score;
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 16.0),
       child: Align(
         alignment: Alignment.bottomCenter,
-        child: Text(
-          asset,
-          style: const TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.w700,
-          ),
+        child: Column(
+          children: [
+            Text(
+             'You will win: $item',
+              style: const TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              'Your score: $score',
+              style: const TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ],
         ),
       ),
     );
